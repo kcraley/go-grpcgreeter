b0e4d2b952141f07aaf2eee77bd5dba432103f06
Keith Craley <kcral21@gmail.com>
2022-05-25 01:10:23 -0400

    allow client to retrieve the greeting message

--- cmd/client.go
+++ cmd/client.go
@@ -1,6 +1,17 @@
 package cmd
 
-import "github.com/urfave/cli/v2"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/urfave/cli/v2"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/kcraley/go-grpcgreeter/greeter"
+)
 
 // newClientCommand creates and returns the 'client' subcommand.
 func newClientCommand() *cli.Command {
@@ -8,13 +19,50 @@ func newClientCommand() *cli.Command {
 		Name:    "client",
 		Aliases: []string{"c"},
 		Usage:   "runs the application in client mode to produce messages to the server.",
-		Action:  newClientAction(),
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "address",
+				Value: "127.0.0.1",
+				Usage: "endpoint where the appliation server is listening.",
+			},
+			&cli.StringFlag{
+				Name:  "name",
+				Value: "world",
+				Usage: "name of the individual to generate the greeting for.",
+			},
+			&cli.StringFlag{
+				Name:  "port",
+				Value: "8080",
+				Usage: "port where the application server is listening.",
+			},
+		},
+		Action: newClientAction(),
 	}
 }
 
 // newClientAction
 func newClientAction() cli.ActionFunc {
-	return func(ctx *cli.Context) error {
+	return func(ctxCli *cli.Context) error {
+		// Setup the grpc connection dialer.
+		addr := fmt.Sprintf("%s:%s", ctxCli.String("address"), ctxCli.String("port"))
+		connection, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			log.Fatalf("unable to create grpc dialer: %v", err)
+			return err
+		}
+		defer connection.Close()
+
+		// Make our call the the server
+		c := pb.NewGreeterClient(connection)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+
+		rep, err := c.SayHello(ctx, &pb.HelloRequest{Name: ctxCli.String("name")})
+		if err != nil {
+			log.Fatalf("unable to greet: %v", err)
+			return err
+		}
+		log.Printf("Greeting Message: %s", rep.GetMessage())
 		return nil
 	}
 }
